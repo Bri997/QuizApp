78365c78bd6f1a4e40695a2a91c6227633f1d0b2
danale <danale@danales-MacBook-Pro.local>
2018-01-21 00:13:32 -0500

    Developed ability to load first question

--- quizJS.js
+++ quizJS.js
@@ -1,59 +1,46 @@
-
 var myQuestions = [
   {
-    question: "What is your nearest star?",
-    answers: {
-      a: "Alpha Centauri",
-      b: "Barnard\'s Star",
-      c: "Sirius",
-      d: "Sol"
+    "question": "What is your nearest star?",
+    "answers": {
+      "a": "Alpha Centauri",
+      "b": "Barnard\'s Star",
+      "c": "Sirius",
+      "d": "Sol"
     },
-    correctAnswer: "d"
+    "correctAnswer": "d"
   },
 
   {
-    question: "It is important to stay away from the event horizon \n of our black hole Sagittarius A* \n that limit is?",
-    answers: {
-      a: "Eight million miles",
-      b: "Fifty thousand miles",
-      c: "Twenty seven trillion miles"
+    "question": "It is important to stay away from the event horizon \n of our black hole Sagittarius A* \n that limit is?",
+    "answers": {
+      "a": "Eight million miles",
+      "b": "Fifty thousand miles",
+      "c": "Twenty seven trillion miles"
     },
-    correctAnswer: "a"
+    "correctAnswer": "a"
   }
-
-// put more questions here
 ];
-/*
-function answerChecker(val) {
-  const rightAnswer = correctAnswer.filter(val){
-    if val === correctAnswer
-    console.log("GOT IT")
-  }
+
+var output = document.getElementById('output');
+    page = 0;
+loadQuestions();
+
+function loadQuestions(){
+  page = 1;
+  buildQuiz(1);
 }
-*/
 
-
-function questionContainer() {
-  for(var i in myQuestions){
-    console.log(myQuestions[i].question);
-      console.log(myQuestions[i].answers);
-      console.log(myQuestions[i].correctAnswer);
-      var questionHolder = "";
-      for ( var i in myQuestions[i].answers){
-        questionHolder += `<div `
-      }
+function buildQuiz(){
+  for (var i in myQuestions) {
+    var myQuestion = myQuestions[page - 1].question;
+    var myCorrect = myQuestions[page - 1].question;
+    var questionHolder = '';
+    for(var i in myQuestions[page - 1].answers){
+      questionHolder += '<div class="col-sm-6"><a>'+myQuestions[page-1].answers[i] + '</a></div>';
     }
-
+    output.innerHTML = '<div>'+ myQuestion + '</div>';
+    output.innerHTML += questionHolder;
+    console.log();
+  }
 
 }
-
-questionContainer();
-
-
-
-/*
-$(".quizContainer").prepend ('Some Question');
-$(".quizSelections").append("Select Sutff");
-
-});
-*/
