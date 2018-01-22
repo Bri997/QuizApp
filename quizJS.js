2037897c87d6d0591b4e0c19385727489c935fe1
danale <danale@danales-MacBook-Pro.local>
2018-01-22 14:05:18 -0500

    Resolving bug with page

--- quizJS.js
+++ quizJS.js
@@ -25,29 +25,33 @@ var output = document.getElementById('output');
     page = 0;
 loadQuestions();
 
+// Event Listeners
+btnPre.onclick = buildQuiz(page - 1);
+// btnNxt.onclick = buildQuiz(page + 1);
+
 function loadQuestions(){
-  page = 1;
   buildQuiz(1);
 }
 
-function buildQuiz(){
-  for (var i in myQuestions) {
-    var myQuestion = myQuestions[page - 1].question;
-    var myCorrect = myQuestions[page - 1].correctAnswer;
-    var questionHolder = '';
-    var yesCor = '';
-    for(var i in myQuestions[page - 1].answers){
-      console.log(i);
-      if (i == myCorrect) {
-        yesCor = '*';
-      } else {
-        yesCor = '';
-      }
-      questionHolder += '<div class="col-sm-6"><div class="btnAns">'+myQuestions[page-1].answers[i] + ' ' +  yesCor + '</div></div>';
+function buildQuiz(pg){
+  console.log(pg);
+  page = pg;
+  console.log(page);
+  var myQuestion = myQuestions[page - 1].question;
+  var myCorrect = myQuestions[page - 1].correctAnswer;
+  var questionHolder = '';
+  var yesCor = '';
+  for(var i in myQuestions[page - 1].answers){
+    console.log(i);
+    if (i == myCorrect) {
+      yesCor = '*';
+    } else {
+      yesCor = '';
     }
+    questionHolder += '<div class="col-sm-6"><div class="btnAns">'+myQuestions[page-1].answers[i] + ' ' +  yesCor + '</div></div>';
+  }
     output.innerHTML = '<div class="myQ">'+ myQuestion + ' </div>';
     output.innerHTML += questionHolder;
     console.log();
-  }
 
 }
