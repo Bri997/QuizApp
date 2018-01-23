f42f05139f932736c090023e596519a5e1405669
danale <danale@danales-MacBook-Pro.local>
2018-01-22 19:18:24 -0500

    this code will be removed

--- quizJS.js
+++ quizJS.js
@@ -1,25 +1,48 @@
-var myQuestions = [
-  {
-    "question": "What is your nearest star?",
-    "answers": {
-      "a": "Alpha Centauri",
-      "b": "Barnard\'s Star",
-      "c": "Sirius",
-      "d": "Sol"
+var myQuestions = {
+  [
+    {
+      "question": "What is your nearest star?",
+      "answers": {
+        "a": "Alpha Centauri",
+        "b": "Barnard\'s Star",
+        "c": "Sirius",
+        "d": "Sol"
+      },
+      "correctAnswer": "d"
     },
-    "correctAnswer": "d"
-  },
 
-  {
-    "question": "It is important to stay away from the event horizon \n of our black hole Sagittarius A* \n that limit is?",
-    "answers": {
-      "a": "Eight million miles",
-      "b": "Fifty thousand miles",
-      "c": "Twenty seven trillion miles"
-    },
-    "correctAnswer": "a"
-  }
-];
+    {
+      "question": "It is important to stay away from the event horizon \n of our black hole Sagittarius A* \n that limit is?",
+      "answers": {
+        "a": "Eight million miles",
+        "b": "Fifty thousand miles",
+        "c": "Twenty seven trillion miles"
+      },
+      "correctAnswer": "a"
+    }
+  ];
+  currentQuestion: 0;
+  totalScore: 0;
+  startQuiz: false;
+}
+
+$(function() {
+  $("#startQuiz").click(function() {
+    startQuiz();
+  });
+  $(".myForm").submit(function(event) {
+    event.preventDefault();
+    nextQuestion();
+  });
+  $("#continue").click(function() {
+    renderPage();
+  });
+  $("#retake").click(function(event) {
+    event.preventDefault();
+    retakeQuiz();
+  });
+  renderPage();
+});
 
 var output = document.getElementById('output');
     page = 0;
@@ -27,31 +50,39 @@ loadQuestions();
 
 // Event Listeners
 btnPre.onclick = buildQuiz(page - 1);
-// btnNxt.onclick = buildQuiz(page + 1);
+btnNxt.onclick = buildQuiz(page + 1);
 
 function loadQuestions(){
   buildQuiz(1);
 }
 
-function buildQuiz(pg){
-  console.log(pg);
-  page = pg;
+function buildQuiz(page){
   console.log(page);
+  if (page > 0) {
+    yesCor = '*';
+  } else {
+     yesCor = '';
+  }
+  questionHolder += '<div class="col-sm-6"><div class="btnAns">'+myQuestions[page-1].answers[i] + ' ' +  yesCor + '</div></div>';
+  }
+    output.innerHTML = '<div class="myQ">'+ myQuestion + ' </div>';
+    output.innerHTML += questionHolder;
+    console.log();
   var myQuestion = myQuestions[page - 1].question;
   var myCorrect = myQuestions[page - 1].correctAnswer;
   var questionHolder = '';
   var yesCor = '';
   for(var i in myQuestions[page - 1].answers){
     console.log(i);
-    if (i == myCorrect) {
-      yesCor = '*';
-    } else {
-      yesCor = '';
-    }
-    questionHolder += '<div class="col-sm-6"><div class="btnAns">'+myQuestions[page-1].answers[i] + ' ' +  yesCor + '</div></div>';
-  }
-    output.innerHTML = '<div class="myQ">'+ myQuestion + ' </div>';
-    output.innerHTML += questionHolder;
-    console.log();
+  //   if (i == myCorrect) {
+  //     yesCor = '*';
+  //   } else {
+  //     yesCor = '';
+  //   }
+  //   questionHolder += '<div class="col-sm-6"><div class="btnAns">'+myQuestions[page-1].answers[i] + ' ' +  yesCor + '</div></div>';
+  // }
+  //   output.innerHTML = '<div class="myQ">'+ myQuestion + ' </div>';
+  //   output.innerHTML += questionHolder;
+  //   console.log();
 
 }
